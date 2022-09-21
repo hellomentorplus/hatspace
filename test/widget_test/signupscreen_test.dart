0942b15fa62cb595067baabe7a29b4e0b0c77976
Duc Vinh Le <vinhxp95@gmail.com>
2022-09-21 22:44:04 +1000

    Update signupscreen_test.dart
    Complete testing widget on sign up screen when showing error message of email and password

--- test/widget_test/signupscreen_test.dart
+++ test/widget_test/signupscreen_test.dart
@@ -7,18 +7,48 @@ import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
 import 'signupscreen_test.mocks.dart';
+
 @GenerateMocks([SignUpCubit])
-void main (){
-  testWidgets("Email error message", (widgetTester)async{
+void main() {
+  Widget widget = const SigUpScreen();
+  final MockSignUpCubit signUpCubit = MockSignUpCubit();
+  final Widget blocWrapper = BlocProvider<SignUpCubit>(
+    create: (context) => signUpCubit,
+    child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+            primaryColor: const Color(0xff006606),
+            textTheme: const TextTheme(
+              displayMedium:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              titleLarge: TextStyle(color: Colors.white),
+              titleMedium:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.w800),
+            ),
+            iconTheme: const IconThemeData(color: Colors.black, size: 15)),
+        home: Builder(
+          builder: (context) => widget,
+        )),
+  );
+  testWidgets("Email error message", (widgetTester) async {
     // await tester.wrapAndPumpWidget(const SigUpScreen());
     Widget widget = const SigUpScreen();
 
     final MockSignUpCubit signUpCubit = MockSignUpCubit();
-    when(signUpCubit.state).thenAnswer((realInvocation) =>const SignUpState(isEmailValid:false,isPasswordValid: true,emailError:"Email is Invalid",passwordError: "") );
-    when(signUpCubit.stream).thenAnswer((realInvocation) => Stream.value(const SignUpState(isEmailValid:false,isPasswordValid: true,emailError:"Email is Invalid",passwordError: "")));
+    when(signUpCubit.state).thenAnswer((realInvocation) => const SignUpState(
+        isEmailValid: false,
+        isPasswordValid: true,
+        emailError: "Email is Invalid",
+        passwordError: ""));
+    when(signUpCubit.stream).thenAnswer((realInvocation) => Stream.value(
+        const SignUpState(
+            isEmailValid: false,
+            isPasswordValid: true,
+            emailError: "Email is Invalid",
+            passwordError: "")));
 
     final Widget blocWrapper = BlocProvider<SignUpCubit>(
-      create: (context)=> signUpCubit,
+      create: (context) => signUpCubit,
       child: MaterialApp(
           title: 'Flutter Demo',
           theme: ThemeData(
@@ -31,19 +61,88 @@ void main (){
                     TextStyle(color: Colors.white, fontWeight: FontWeight.w800),
               ),
               iconTheme: const IconThemeData(color: Colors.black, size: 15)),
-          home: Builder(builder: (context)=>widget,)
-        ),
-      );
+          home: Builder(
+            builder: (context) => widget,
+          )),
+    );
 
     await widgetTester.pumpWidget(blocWrapper);
     // await widgetTester.pumpFrames(blocWrapper, const Duration(milliseconds: 160));
     // await tester.pumpFrames(const SigUpScreen(),const Duration(milliseconds: 160));
-    Iterable <TextField> textFieldList = widgetTester.widgetList(find.byType(TextField));
+    Iterable<TextField> textFieldList =
+        widgetTester.widgetList(find.byType(TextField));
     TextField emailTextField = textFieldList.first;
     expect(emailTextField.decoration?.errorText, "Email is Invalid");
-
   });
 
+  testWidgets("Test Error Message when password is too short error message",
+      (widgetTester) async {
+    // await tester.wrapAndPumpWidget(const SigUpScreen());
+    SignUpState state = const SignUpState(
+        isEmailValid: true,
+        isPasswordValid: false,
+        emailError: "",
+        passwordError: "Password is too short");
+    when(signUpCubit.state).thenAnswer((realInvocation) => state);
+    when(signUpCubit.stream)
+        .thenAnswer((realInvocation) => Stream.value(state));
+
+    await widgetTester.pumpWidget(blocWrapper);
+    // await widgetTester.pumpFrames(blocWrapper, const Duration(milliseconds: 160));
+    // await tester.pumpFrames(const SigUpScreen(),const Duration(milliseconds: 160));
+    Iterable<TextField> textFieldList =
+        widgetTester.widgetList(find.byType(TextField));
+    TextField emailTextField = textFieldList.last;
+    expect(emailTextField.decoration?.errorText, "Password is too short");
+  });
+
+  testWidgets("When password missing uppercase character ", (widgetTester) async {
+    SignUpState state = const SignUpState(
+        isEmailValid: true,
+        isPasswordValid: false,
+        emailError: "",
+        passwordError: "Password needs at least 1 uppercase character");
+    when(signUpCubit.state).thenAnswer((realInvocation) => state);
+    when(signUpCubit.stream)
+        .thenAnswer((realInvocation) => Stream.value(state));
+
+    await widgetTester.pumpWidget(blocWrapper);
+    Iterable<TextField> textFieldList =
+        widgetTester.widgetList(find.byType(TextField));
+    TextField emailTextField = textFieldList.last;
+    expect(emailTextField.decoration?.errorText, "Password needs at least 1 uppercase character");
+  });
+  testWidgets("When password missing lowercase lo character ", (widgetTester) async {
+    SignUpState state = const SignUpState(
+        isEmailValid: true,
+        isPasswordValid: false,
+        emailError: "",
+        passwordError: "Password needs at least 1 lowercase character");
+    when(signUpCubit.state).thenAnswer((realInvocation) => state);
+    when(signUpCubit.stream)
+        .thenAnswer((realInvocation) => Stream.value(state));
+
+    await widgetTester.pumpWidget(blocWrapper);
+    Iterable<TextField> textFieldList =
+        widgetTester.widgetList(find.byType(TextField));
+    TextField emailTextField = textFieldList.last;
+    expect(emailTextField.decoration?.errorText, "Password needs at least 1 lowercase character");
+  });
+
+   testWidgets("When password missing number ", (widgetTester) async {
+    SignUpState state = const SignUpState(
+        isEmailValid: true,
+        isPasswordValid: false,
+        emailError: "",
+        passwordError: "Password needs at least 1 number");
+    when(signUpCubit.state).thenAnswer((realInvocation) => state);
+    when(signUpCubit.stream)
+        .thenAnswer((realInvocation) => Stream.value(state));
+
+    await widgetTester.pumpWidget(blocWrapper);
+    Iterable<TextField> textFieldList =
+        widgetTester.widgetList(find.byType(TextField));
+    TextField emailTextField = textFieldList.last;
+    expect(emailTextField.decoration?.errorText, "Password needs at least 1 number");
+  });
 }
-
-
