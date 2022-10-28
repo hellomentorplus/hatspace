23f9acd4bf8cba4b1e8e6f3aacae3b74b41e2a50
suesitran <17781268+suesitran@users.noreply.github.com>
2022-10-28 17:57:57 +1100

    add firebase and bloc implementation

---
+++ lib/features/home/view/home_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePageView extends StatelessWidget {
+  const HomePageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: const Center(
+        child: Text('T.B.D'),
+      ),
+    );
+  }
+}
