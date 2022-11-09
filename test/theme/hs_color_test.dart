7b73b20f3aeb84054e2527abd9863a68bebf6501
tai duong <asoi1990@hotmail.com>
2022-11-09 22:03:43 +1100

    theme color and text theme

---
+++ test/theme/hs_color_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hatspace/features/home/view/home_view.dart';
+
+import 'package:hatspace/main.dart';
+
+import '../widget_tester_extension.dart';
+
+void main() {
+  testWidgets('Check home screen title', (WidgetTester tester) async {
+    //   const widget = HomePageView();
+    //   await tester.wrapAndPump(widget);
+
+    //   // // Verify that our counter starts at 0.
+    //   expect(find.text('HAT Space'), findsOneWidget);
+  });
+}
