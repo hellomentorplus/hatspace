8866a71fb631cf21a8d5d72bf82d06355848b864
tai duong <asoi1990@hotmail.com>
2022-11-09 22:03:43 +1100

    theme color and text theme

---
+++ lib/theme/hs_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+part 'hs_color.dart';
+part 'hs_text_theme.dart';
+
+ThemeData themeData = ThemeData(
+    textTheme: textTheme,
+    colorScheme: colorScheme,
+    fontFamily: 'Be Vietnam Pro');
