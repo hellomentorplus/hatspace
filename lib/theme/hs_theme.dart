c5d0c0441b3b4be2631dc8f440ff589ec76155d1
tai duong <asoi1990@hotmail.com>
2022-11-11 22:06:39 +1100

    update theme color

--- lib/theme/hs_theme.dart
+++ lib/theme/hs_theme.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 part 'hs_color.dart';
-part 'hs_text_theme.dart';
+// part 'hs_text_theme.dart';
 
 ThemeData lightThemeData = ThemeData(
-    textTheme: textTheme,
+    // textTheme: textTheme,
     colorScheme: colorScheme,
     fontFamily: 'Be Vietnam Pro');
