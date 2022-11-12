0ae7f00973facfc25cd684685cc0b46db39c192d
tai duong <asoi1990@hotmail.com>
2022-11-12 11:31:09 +1100

    update merge

--- lib/theme/hs_theme.dart
+++ lib/theme/hs_theme.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
 
 part 'hs_color.dart';
@@ -8,6 +7,3 @@ ThemeData lightThemeData = ThemeData(
     // textTheme: textTheme,
     colorScheme: colorScheme,
     fontFamily: 'Be Vietnam Pro');
-=======
-// TODO implement theme data here
->>>>>>> 69e12225b960a91a0ac9f190355174823aa371aa
