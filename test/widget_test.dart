9c59b0cbcaaed681123e460208aca02a626d736b
tai duong <asoi1990@hotmail.com>
2022-11-13 16:44:02 +1100

    update unit test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,15 +10,27 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:hatspace/features/home/view/home_view.dart';
 
 import 'package:hatspace/main.dart';
+import 'package:hatspace/theme/hs_theme.dart';
 
 import 'widget_tester_extension.dart';
 
 void main() {
   testWidgets('Check home screen title', (WidgetTester tester) async {
     const widget = HomePageView();
+
     await tester.wrapAndPump(widget);
 
     // // Verify that our counter starts at 0.
     expect(find.text('HAT Space'), findsOneWidget);
+  });
+
+  testWidgets('It should be in the landscape view', (tester) async {
+    const widget = MyApp();
+
+    await tester.wrapAndPump(widget, theme: lightThemeData);
+
+    final renderingWidget = tester.widget(find.byType(HomePageView));
+
+    expect(renderingWidget, isA<Widget>());
   });
 }
