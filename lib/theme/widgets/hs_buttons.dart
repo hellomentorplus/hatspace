8af6e7d9c67074d54c87fabd785a6a11883c1082
dlvinh <vinhxp95@gmail.com>
2022-11-14 00:13:00 +1100

    add button theme
    Add: -Button theme and Share button
             - Test coverage of button theme

---
+++ lib/theme/widgets/hs_buttons.dart
@@ -0,0 +1,120 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/material.dart';
+import 'package:hatspace/theme/hs_button_theme.dart';
+import 'package:hatspace/theme/hs_theme.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final String? label;
+  final String? iconUrl;
+  final VoidCallback? onPressed;
+
+  const PrimaryButton({Key? key, this.label, this.iconUrl, this.onPressed})
+      : assert(label != null || iconUrl == null, "label or icon is required"),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // FOR TEXT ONLY BUTTON
+    if (iconUrl == null && label != null) {
+      return ElevatedButton(
+          key: key,
+          onPressed: onPressed,
+          style: primaryButtonTheme.style,
+          child: Text(label!));
+    }
+    // BUTTON WITH ICON ONLY
+    if (iconUrl != null && label == null) {
+      // DESIGN BUTTON WITH ICON HERE
+    }
+    // Button with icon
+    return ElevatedButton(
+        key: key,
+        onPressed: onPressed,
+        style: buttonWithIconTheme.style,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16),
+              child: SvgPicture.asset(
+                iconUrl!,
+                width: 24,
+                height: 24,
+                color: onPressed == null ? HSColor.onPrimary : null,
+                alignment: Alignment.center,
+              ),
+            ),
+            Expanded(
+                flex: 4,
+                child: Text(
+                  label!,
+                  textAlign: TextAlign.center,
+                )),
+          ],
+        ));
+  }
+}
+
+class SecondaryButton extends StatelessWidget {
+  final String? label;
+  final String? iconURL;
+  final VoidCallback? onPressed;
+  const SecondaryButton({Key? key, this.label, this.iconURL, this.onPressed})
+      : assert(label != null || iconURL != null, "either are required"),
+        super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    // WITH TEXT ONLY
+    if (iconURL == null && label != null) {
+      return OutlinedButton(
+          onPressed: onPressed,
+          style: secondaryButtonTheme.style,
+          child: Text(label!));
+    }
+    if (iconURL != null && label == null) {
+      // IMPLEMENT ONLY ICON HERE
+    }
+
+    // BUTTON WITH ICON AND TEXT
+    return OutlinedButton(
+        onPressed: onPressed,
+        style: secondaryButtonWithIconTheme.style,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.only(left: 16, right: 16),
+                child: SvgPicture.asset(
+                  iconURL!,
+                  width: 24,
+                  height: 24,
+                  color: onPressed == null ? HSColor.neutral3 : null,
+                  alignment: Alignment.center,
+                )),
+            Expanded(
+                flex: 4,
+                child: Text(
+                  label!,
+                  textAlign: TextAlign.center,
+                )),
+          ],
+        ));
+  }
+}
+
+class TextOnlyButton extends StatelessWidget {
+  final String label;
+  final VoidCallback? onPressed;
+  const TextOnlyButton({Key? key, required this.label, required this.onPressed})
+      : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: textOnlyButtonTheme.style,
+      child: Text(label),
+    );
+  }
+}
