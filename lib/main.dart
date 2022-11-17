4108c4fd5e34453bcf05c6b63f8e70421c39deda
tai duong <asoi1990@hotmail.com>
2022-11-17 17:48:59 +1100

    update fonts in theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hatspace/firebase_options.dart';
-import 'package:hatspace/main_app.dart';
+import 'package:hatspace/initial_app.dart';
 import 'package:hatspace/singleton/hs_singleton.dart';
 
 void main() async {
--- lib/theme/hs_theme.dart
+++ lib/theme/hs_theme.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hatspace/gen/assets.gen.dart';
+import 'package:hatspace/gen/fonts.gen.dart';
 
 part 'hs_color.dart';
 part 'hs_text_theme.dart';
 
 ThemeData lightThemeData = ThemeData(
-    textTheme: textTheme, colorScheme: colorScheme, fontFamily: 'BeVietnamPro');
+    textTheme: textTheme,
+    colorScheme: colorScheme,
+    fontFamily: FontFamily.beVietnamPro);
