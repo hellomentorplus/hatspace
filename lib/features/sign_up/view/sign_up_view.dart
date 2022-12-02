5bf58e8543083d2e9bee60b956017c08ee77335d
dlvinh <vinhxp95@gmail.com>
2022-12-02 18:23:48 +1100

    Update sign up
    Remove Material App in file

--- lib/features/sign_up/view/sign_up_view.dart
+++ lib/features/sign_up/view/sign_up_view.dart
@@ -16,101 +16,87 @@ class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        localizationsDelegates: const [
-          HatSpaceStrings.delegate, // Add this line
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        home: Scaffold(
-            appBar: AppBar(
-                elevation: 0, // Remove shadow from app bar background
-                backgroundColor: HSColor.background,
-                leading: Padding(
-                  padding: const EdgeInsets.only(top: 0, left: 10, right: 0),
-                  child: IconButton(
-                    icon: SvgPicture.asset(Assets.images.closeIcon),
-                    onPressed: () {
-                      // ON PRESSED EVENT HERE
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HomePageView()));
-                    },
-                  ),
-                )),
-            body: Padding(
-                padding: const EdgeInsets.only(top: 60, left: 16, right: 16),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: [
-                    Expanded(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                          Padding(
-                              padding: const EdgeInsets.only(bottom: 16),
-                              child: SecondaryButton(
-                                label:
-                                    HatSpaceStrings.of(context)!.googleSignUp,
-                                iconURL: Assets.images.google,
-                                onPressed: () {},
-                              )),
-                          Padding(
-                              padding: const EdgeInsets.only(bottom: 16),
-                              child: SecondaryButton(
-                                label:
-                                    HatSpaceStrings.of(context)!.facebookSignUp,
-                                iconURL: Assets.images.facebookround,
-                                onPressed: () {},
-                              )),
-                          Padding(
-                              padding: const EdgeInsets.only(bottom: 18),
-                              child: SecondaryButton(
-                                label: HatSpaceStrings.of(context)!.emailSignUp,
-                                iconURL: Assets.images.envelope,
-                                onPressed: () {},
-                              )),
-                          Padding(
-                              padding: const EdgeInsets.only(bottom: 52),
-                              child: RichText(
-                                  text: TextSpan(
-                                      style: textTheme.bodyMedium?.copyWith(
-                                          fontSize: 12,
-                                          color: HSColor.onSurface),
-                                      children: [
-                                    TextSpan(
-                                        style: textTheme.bodyMedium?.copyWith(
-                                            fontSize: 12,
-                                            color: HSColor.onSurface),
-                                        text: "Already have account ? "),
-                                    TextSpan(
-                                        style: const TextStyle(
-                                            fontWeight: FontWeight.w600,
-                                            decoration:
-                                                TextDecoration.underline),
-                                        text: "Sign in",
-                                        recognizer: TapGestureRecognizer()
-                                          ..onTapDown = (details) {
-                                            print("Tap down event");
-                                          })
-                                  ]))),
-                          TextOnlyButton(
-                            label: "Skip",
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: ((context) => HomePageView())));
-                            },
-                          ),
-                          const SizedBox(
-                            height: 71,
-                          )
-                        ]))
-                  ],
-                ))));
+    return Scaffold(
+        appBar: AppBar(
+            elevation: 0, // Remove shadow from app bar background
+            backgroundColor: HSColor.background,
+            leading: Padding(
+              padding: const EdgeInsets.only(top: 0, left: 10, right: 0),
+              child: IconButton(
+                icon: SvgPicture.asset(Assets.images.closeIcon),
+                onPressed: () {
+                  // ON PRESSED EVENT HERE
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => HomePageView()));
+                },
+              ),
+            )),
+        body: Padding(
+            padding: const EdgeInsets.only(top: 60, left: 16, right: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 16),
+                          child: SecondaryButton(
+                            label: HatSpaceStrings.of(context)!.googleSignUp,
+                            iconURL: Assets.images.google,
+                            onPressed: () {},
+                          )),
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 16),
+                          child: SecondaryButton(
+                            label: HatSpaceStrings.of(context)!.facebookSignUp,
+                            iconURL: Assets.images.facebookround,
+                            onPressed: () {},
+                          )),
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 18),
+                          child: SecondaryButton(
+                            label: HatSpaceStrings.of(context)!.emailSignUp,
+                            iconURL: Assets.images.envelope,
+                            onPressed: () {},
+                          )),
+                      Padding(
+                          padding: const EdgeInsets.only(bottom: 52),
+                          child: RichText(
+                              text: TextSpan(
+                                  style: textTheme.bodyMedium?.copyWith(
+                                      fontSize: 12, color: HSColor.onSurface),
+                                  children: [
+                                TextSpan(
+                                    style: textTheme.bodyMedium?.copyWith(
+                                        fontSize: 12, color: HSColor.onSurface),
+                                    text: "Already have account ? "),
+                                TextSpan(
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        decoration: TextDecoration.underline),
+                                    text: "Sign in",
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTapDown = (details) {
+                                        print("Tap down event");
+                                      })
+                              ]))),
+                      TextOnlyButton(
+                        label: "Skip",
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: ((context) => HomePageView())));
+                        },
+                      ),
+                      const SizedBox(
+                        height: 71,
+                      )
+                    ]))
+              ],
+            )));
   }
 }
