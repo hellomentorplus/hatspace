8b2e85ce357d09cf8a01c616c2b0f10f6854d3a5
Duc Vinh Le <85751505+dlvinh@users.noreply.github.com>
2023-03-13 21:56:06 +1100

    [Hs71] - Toast message part 1 (#35)
    * hs71 - add UI error toast theme
    
    * hs 71- add snackbar and show toast with snackbar
    
    * hs71 - toast message update
    
    - Scenario1: add toast message to widget catalog
    - Scenario2: dismiss after 6 secs
    - Scen3 : clickable close button
    
    * Update widget accordingly
    
    Resolve lead comments
    
    * Update toast widget with enum type
    
    * Add unit test for widget
    
    -Change file name from
    "toast_messages" to "toast_messages_extension"
    
    * Fixed code follow comments
    
    * Update format
    
    * update and fix code
    
    Update unit test
    remove function
    
    * remove choosing role

---
+++ lib/theme/toast_messages/hs_toast_theme.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hatspace/gen/assets.gen.dart';
+import 'package:hatspace/theme/toast_messages/toast_messages_extension.dart';
+
+const maxLength = 160;
+const maxLines = 2;
+// TODO: Need to update when having update new theme
+double containerPadding = 12.0;
+double iconSize = 24.0;
+
+enum ToastType {
+  errorToast(backgroundColor: Color.fromARGB(255, 255, 241, 241));
+
+  final Color backgroundColor;
+  String get icon => _getIcon();
+  String get closeIcon => _getCloseIcon();
+
+  const ToastType({required this.backgroundColor});
+
+  String _getIcon() {
+    switch (this) {
+      case ToastType.errorToast:
+        return Assets.images.error;
+    }
+  }
+
+  String _getCloseIcon() {
+    switch (this) {
+      case ToastType.errorToast:
+        return Assets.images.closeIcon;
+    }
+  }
+}
+
+class ToastMessageContainer extends StatelessWidget {
+  final ToastType toastType;
+  final String toastTitle;
+  final String toastContent;
+  const ToastMessageContainer({
+    super.key,
+    required this.toastType,
+    required this.toastTitle,
+    required this.toastContent,
+  })  : assert(toastContent.length <= maxLength,
+            "Toast content from `$toastContent` must not over 160 characters"),
+        assert(toastTitle.length <= maxLength,
+            "Toast title `$toastTitle must not over 160 charaters ");
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(containerPadding),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            color: toastType.backgroundColor),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            SvgPicture.asset(
+              toastType.icon,
+              width: iconSize,
+              height: iconSize,
+            ),
+            Flexible(
+                child: Padding(
+              padding: EdgeInsets.only(left: containerPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          toastTitle,
+                          maxLines: maxLines,
+                          overflow: TextOverflow.ellipsis,
+                          // TODO: TextStyle-Color need to be updated when new theme released
+                          style: const TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 14,
+                            color: Color(0xff141414),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                          key: const Key("closeTap"),
+                          onTap: () {
+                            ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                          },
+                          child: SvgPicture.asset(
+                            toastType.closeIcon,
+                            width: iconSize,
+                            height: iconSize,
+                          ))
+                    ],
+                  ),
+                  Text(toastContent,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: maxLines,
+                      // TODO: TextStyle-Color need to be updated when new theme released
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12,
+                          color: Color(0xff383838))),
+                ],
+              ),
+            )),
+          ],
+        ));
+  }
+}
---
+++ lib/theme/toast_messages/toast_messages_extension.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:hatspace/theme/toast_messages/hs_toast_theme.dart';
+
+extension ToastMessagesExtension on BuildContext {
+  void showToast(ToastType type, String title, String message) {
+    ScaffoldMessenger.of(this).showSnackBar(
+      SnackBar(
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Padding(
+              padding: const EdgeInsets.only(top: 12),
+              child: ToastMessageContainer(
+                  toastType: type, toastTitle: title, toastContent: message))
+        ],
+      ),
+      backgroundColor: Colors.transparent,
+      behavior: SnackBarBehavior.floating,
+      elevation: 0,
+      duration: const Duration(seconds: 6),
+    ));
+  }
+}
