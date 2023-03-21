991c08db11b03262d9881facd40ff40165956971
suesitran <17781268+suesitran@users.noreply.github.com>
2023-03-21 23:58:23 +1100

    create storage service for members

---
+++ lib/models/storage/storage_service.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+
+import 'member_service/member_storage_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class StorageService {
+  static FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  MemberService _member = MemberService(_firestore);
+
+  MemberService get member => _member;
+
+  @protected
+  @visibleForTesting
+  static set firestore(FirebaseFirestore firestore) => _firestore = firestore;
+}
