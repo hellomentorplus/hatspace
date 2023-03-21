289975f19169db23457455829198174f390ced29
suesitran <17781268+suesitran@users.noreply.github.com>
2023-03-22 00:17:58 +1100

    [HS-72] add test to member storage service

---
+++ test/models/storage/member_storage_service_test.dart
@@ -0,0 +1,107 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hatspace/data/data.dart';
+import 'package:hatspace/models/storage/member_service/member_storage_service.dart';
+import 'package:hatspace/models/storage/storage_service.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'member_storage_service_test.mocks.dart';
+
+@GenerateMocks([FirebaseFirestore, CollectionReference, DocumentReference, DocumentSnapshot])
+void main() {
+  final MockFirebaseFirestore firestore = MockFirebaseFirestore();
+  final MockCollectionReference<Map<String, dynamic>> collectionReference = MockCollectionReference();
+  final MockDocumentReference<Map<String, dynamic>> documentReference = MockDocumentReference();
+  final MockDocumentSnapshot<Map<String, dynamic>> documentSnapshot = MockDocumentSnapshot();
+
+  setUpAll(() {
+    StorageService.firestore = firestore;
+  });
+
+  setUp(() {
+    when(firestore.collection(any)).thenAnswer((realInvocation) => collectionReference);
+    when(collectionReference.doc(any)).thenAnswer((realInvocation) => documentReference);
+    when(documentReference.get(any)).thenAnswer((realInvocation) => Future.value(documentSnapshot));
+  });
+
+  tearDown(() {
+    reset(firestore);
+    reset(collectionReference);
+    reset(documentReference);
+    reset(documentSnapshot);
+  });
+
+  test('given user roles does not exist, when getUserRoles, then return empty list', () async {
+    when(documentSnapshot.exists).thenAnswer((realInvocation) => false);
+
+    StorageService storageService = StorageService();
+
+    final result = await storageService.member.getUserRoles('uid');
+
+    expect(result.isEmpty, isTrue);
+  });
+
+  test('given user roles data is null, when getUserRoles, then return empty list', () async {
+    when(documentSnapshot.exists).thenAnswer((realInvocation) => true);
+    when(documentSnapshot.data()).thenAnswer((realInvocation) => null);
+
+    StorageService storageService = StorageService();
+
+    final result = await storageService.member.getUserRoles('uid');
+
+    expect(result.isEmpty, isTrue);
+  });
+
+  test('given user roles data does not contain roles, when getUserRoles, then return empty list', () async {
+    when(documentSnapshot.exists).thenAnswer((realInvocation) => true);
+    when(documentSnapshot.data()).thenAnswer((realInvocation) => {});
+
+    StorageService storageService = StorageService();
+
+    final result = await storageService.member.getUserRoles('uid');
+
+    expect(result.isEmpty, isTrue);
+  });
+
+  test('given user roles data is invalid, when getUserRoles, then return empty list', () async {
+    when(documentSnapshot.exists).thenAnswer((realInvocation) => true);
+    when(documentSnapshot.data()).thenAnswer((realInvocation) => {
+      'roles' : 'this is invalid data'
+    });
+
+    StorageService storageService = StorageService();
+
+    final result = await storageService.member.getUserRoles('uid');
+
+    expect(result.isEmpty, isTrue);
+  });
+
+  test('given user roles data is valie, when getUserRoles, then return list of Roles', () async {
+    when(documentSnapshot.exists).thenAnswer((realInvocation) => true);
+    when(documentSnapshot.data()).thenAnswer((realInvocation) => {
+      'roles' : ['tenant', 'homeowner']
+    });
+
+    StorageService storageService = StorageService();
+
+    final result = await storageService.member.getUserRoles('uid');
+
+    expect(result.isEmpty, isFalse);
+    expect(result.length, 2);
+    expect(result.first, Roles.tenant);
+    expect(result.last, Roles.homeowner);
+  });
+
+  test('verify API calls when saveUserRoles', () async {
+    StorageService storageService = StorageService();
+
+    await storageService.member.saveUserRoles('uid', Roles.values);
+
+    verify(firestore.collection('members')).called(1);
+    verify(collectionReference.doc('uid')).called(1);
+    verify(documentReference.set({
+      'roles' : ['tenant', 'homeowner']
+    })).called(1);
+  });
+}
