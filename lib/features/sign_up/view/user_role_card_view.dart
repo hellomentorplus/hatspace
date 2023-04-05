38d7282e79b12692cd7c5e723a5588293330a8d6
dlvinh <vinhxp95@gmail.com>
2023-04-05 20:23:05 +1000

    implement bloc to ui

--- lib/features/sign_up/view/user_role_card_view.dart
+++ lib/features/sign_up/view/user_role_card_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hatspace/features/sign_up/view_model/choose_role_view/choose_role_view_bloc.dart';
 import 'package:hatspace/strings/l10n.dart';
 import 'package:hatspace/theme/hs_theme.dart';
 
@@ -20,71 +22,50 @@ extension RoleToDetail on Roles {
     return UserRoleDetail(title, description);
   }
 }
-
-class UserRoleCardView extends StatefulWidget {
-  final ValueChanged<bool> onChanged;
-  const UserRoleCardView(
-      {Key? key, required this.position, required this.onChanged})
-      : super(key: key);
-
+class UserRoleCardView extends StatelessWidget {
   final int position;
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _UserRoleCardState createState() => _UserRoleCardState();
-}
-
-class _UserRoleCardState extends State<UserRoleCardView> {
-  ValueNotifier<Set<int>> listenValue = ValueNotifier({});
-
+  const UserRoleCardView({super.key, required this.position});
   @override
   Widget build(BuildContext context) {
     UserRoleDetail userRoleDetail =
-        Roles.values[widget.position].toUserRoleDetail(context);
+        Roles.values[position].toUserRoleDetail(context);
 
-    return InkWell(
-        onTap: () {
-          final bool selectRoleState;
-          Set<int> cur = Set.of(listenValue.value);
-          if (cur.contains(widget.position)) {
-            cur.remove(widget.position);
-            selectRoleState = false;
-          } else {
-            cur.add(widget.position);
-            selectRoleState = true;
-          }
-          listenValue.value = cur;
-          widget.onChanged(selectRoleState);
-        },
-        child: ValueListenableBuilder<Set<int>>(
-          builder: (BuildContext context, Set<int> value, Widget? child) {
-            return Card(
-                margin: const EdgeInsets.only(top: 16),
-                elevation: 6,
-                color: value.contains(widget.position)
-                    ? HSColor.neutral5
-                    : HSColor.neutral1,
-                shape: const RoundedRectangleBorder(
-                  side: BorderSide(color: HSColor.neutral5),
-                  borderRadius: BorderRadius.all(Radius.circular(12)),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        userRoleDetail.title,
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      Text(userRoleDetail.description,
-                          style: Theme.of(context).textTheme.labelSmall)
-                    ],
-                  ),
-                ));
+    return BlocBuilder<ChooseRoleViewBloc, ChooseRoleViewState>(
+        builder: (context, state) {
+      // print(state); ask Sue
+      return InkWell(
+          onTap: () {
+            context
+                .read<ChooseRoleViewBloc>()
+                .add(OnChangeUserRoleEvent(position));
           },
-          valueListenable: listenValue,
-        ));
+          child: Card(
+              margin: const EdgeInsets.only(top: 16),
+              elevation: 6,
+              color: state is UserRoleSelectedListState
+                  ? state.listRole.contains(Roles.values[position])
+                      ? HSColor.neutral5
+                      : HSColor.neutral1
+                  : null,
+              shape: const RoundedRectangleBorder(
+                side: BorderSide(color: HSColor.neutral5),
+                borderRadius: BorderRadius.all(Radius.circular(12)),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      userRoleDetail.title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    Text(userRoleDetail.description,
+                        style: Theme.of(context).textTheme.labelSmall)
+                  ],
+                ),
+              )));
+    });
   }
 }
