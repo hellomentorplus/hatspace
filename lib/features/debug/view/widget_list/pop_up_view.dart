1153b42cd43480cfa05c106388be06ffb74736b2
dlvinh <vinhxp95@gmail.com>
2023-04-09 00:29:47 +1000

    loading with animation implementation

--- lib/features/debug/view/widget_list/pop_up_view.dart
+++ lib/features/debug/view/widget_list/pop_up_view.dart
@@ -1,59 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:hatspace/theme/pop_up/pop_up_controller.dart';
 
-Animatable<Color?> bgColor = TweenSequence<Color?>([
-  TweenSequenceItem(
-    weight: 1.0,
-    tween: ColorTween(
-            begin: const Color.fromARGB(255, 50, 168, 84),
-                       end: const Color.fromARGB(125, 50, 168, 84)),
-  ),
-   TweenSequenceItem(
-    weight: 1.0,
-    tween: ColorTween(
-            begin: const Color.fromARGB(255, 50, 168, 84),
-                       end: const Color.fromARGB(125, 50, 168, 84)),
-  ),
- TweenSequenceItem(
-    weight: 1.0,
-    tween: ColorTween(
-            begin: const Color.fromARGB(255, 50, 168, 84),
-                       end: const Color.fromARGB(125, 50, 168, 84)),
-  ),
-]);
-class PopupView extends StatefulWidget {
+class PopupView extends StatelessWidget {
   const PopupView({super.key});
-
-  @override
-  State<PopupView> createState() => _ProgressIndicatorExampleState();
-}
-
-class _ProgressIndicatorExampleState extends State<PopupView>
-    with TickerProviderStateMixin {
-  late AnimationController controller;
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
+      body: Center(
       child: TextButton(
         onPressed: () {
-          controller = AnimationController(
-            duration: const Duration(seconds: 2),
-            vsync: this,
-          );
-          controller.repeat();
           context.showLoading();
         },
         child: const Text('Show Dialog'),
@@ -61,4 +17,3 @@ class _ProgressIndicatorExampleState extends State<PopupView>
     ));
   }
 }
-
--- lib/theme/pop_up/pop_up.dart
+++ lib/theme/pop_up/pop_up.dart
@@ -3,20 +3,18 @@ import 'package:hatspace/theme/hs_theme.dart';
 
 class PopUp extends StatefulWidget {
   const PopUp({super.key});
-
   @override
   State<PopUp> createState() => _PopUpState();
 }
 
-class _PopUpState extends State<PopUp>
-    with TickerProviderStateMixin {
+class _PopUpState extends State<PopUp> with TickerProviderStateMixin {
   late AnimationController controller;
-  
+
   @override
   void initState() {
     controller = AnimationController(
-            duration: const Duration(seconds: 2),
-            vsync: this,
+      duration: const Duration(seconds: 2),
+      vsync: this,
     );
     super.initState();
   }
@@ -29,27 +27,40 @@ class _PopUpState extends State<PopUp>
 
   @override
   Widget build(BuildContext context) {
+    controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    );
+    controller.repeat();
     return AlertDialog(
-  shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.all(Radius.circular(8.0))),
-  title: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: const [
-      CircularProgressIndicator(
-        backgroundColor: Color.fromARGB(124, 151, 151, 151),
-        strokeWidth: 5,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0))),
+      title: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          AnimatedBuilder(
+              animation: controller,
+              builder: (context, child) {
+                return CircularProgressIndicator(
+                  value: controller.value,
+                  valueColor: controller.drive(ColorTween(
+                      begin: const Color.fromARGB(255, 50, 168, 84),
+                      end: const Color.fromARGB(125, 50, 168, 84))),
+                  // backgroundColor: Color.fromARGB(125,50, 168, 84),
+                  strokeWidth: 5,
+                );
+              }),
+          const SizedBox(
+            height: 16.0,
+          ),
+          const Text("Loading...",
+              style: TextStyle(
+                  //Todo: Change style when material change
+                  color: Color.fromARGB(255, 20, 20, 20),
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14))
+        ],
       ),
-      SizedBox(
-        height: 16.0,
-      ),
-      Text("Loading...",style: TextStyle(
-        //Todo: Change style when material change
-        color: Color.fromARGB(255,20, 20, 20),
-        fontWeight: FontWeight.w500,
-        fontSize: 14
-      ))
-    ],
-  ),
-);
+    );
   }
 }
