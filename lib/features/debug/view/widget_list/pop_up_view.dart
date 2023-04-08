596dc8d148d4e4ea0988da17c31731d3e68db31d
dlvinh <vinhxp95@gmail.com>
2023-04-08 13:19:08 +1000

    pop-up with spinning speed

--- lib/features/debug/view/widget_list/pop_up_view.dart
+++ lib/features/debug/view/widget_list/pop_up_view.dart
@@ -1,5 +1,25 @@
 import 'package:flutter/material.dart';
 
+Animatable<Color?> bgColor = TweenSequence<Color?>([
+  TweenSequenceItem(
+    weight: 1.0,
+    tween: ColorTween(
+            begin: const Color.fromARGB(255, 50, 168, 84),
+                       end: const Color.fromARGB(125, 50, 168, 84)),
+  ),
+   TweenSequenceItem(
+    weight: 1.0,
+    tween: ColorTween(
+            begin: const Color.fromARGB(255, 50, 168, 84),
+                       end: const Color.fromARGB(125, 50, 168, 84)),
+  ),
+ TweenSequenceItem(
+    weight: 1.0,
+    tween: ColorTween(
+            begin: const Color.fromARGB(255, 50, 168, 84),
+                       end: const Color.fromARGB(125, 50, 168, 84)),
+  ),
+]);
 class PopupView extends StatefulWidget {
   const PopupView({super.key});
 
@@ -28,36 +48,41 @@ class _ProgressIndicatorExampleState extends State<PopupView>
       child: TextButton(
         onPressed: () {
           controller = AnimationController(
-            duration: const Duration(seconds: 1),
+            duration: const Duration(seconds: 2),
             vsync: this,
           );
           controller.repeat();
           showDialog<String>(
             barrierDismissible: false,
             context: context,
-            builder: (BuildContext context) => AlertDialog(
-              title: Center(
-                 child: Column(
+            builder: (BuildContext context) => Dialog(
+              child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 AnimatedBuilder(
                     animation: controller,
                     builder: (context, child) {
-                      return CircularProgressIndicator(
+                      return  CircularProgressIndicator(
+                        backgroundColor:Color.fromARGB(124, 151, 151, 151) ,
                         value: controller.value,
-                        strokeWidth: 10,
+                        strokeWidth: 5,
                         valueColor: controller.drive(ColorTween(
                             begin: const Color.fromARGB(255, 50, 168, 84),
                             end: const Color.fromARGB(125, 50, 168, 84))),
                       );
                     },
                 ),
+                const SizedBox(
+                  height: 16,
+                ),
                 const Text("Loading...")
               ],
-            )
+       
               )
                ),
-          ).then((value) => controller.dispose());
+          ).then((value) {
+            controller.dispose();
+          });
         },
         child: const Text('Show Dialog'),
       ),
