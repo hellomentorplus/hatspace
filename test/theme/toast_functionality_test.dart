8c316f2623597f269dea53abe9e2764c882710f1
Duc Vinh Le <85751505+dlvinh@users.noreply.github.com>
2023-04-11 20:44:53 +1000

    Hs107 fix bug UI toast message (#48)
    * Replace Snackbar with Overlay
    
    * fix bug- update toast message extension
    
    * update toast theme with onTap interaction and fix toast extenstion
    
    * update flutter extension
    
    * update test coverage
    
    * fix bug-toast view
    
    * replace close icon for error toast message
    
    * update localisation for error toast
    
    * fix bug - remove yellow underline of text
    
    * format file

---
+++ test/theme/toast_functionality_test.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hatspace/theme/toast_messages/hs_toast_theme.dart';
+import 'package:hatspace/theme/toast_messages/toast_messages_extension.dart';
+
+import '../widget_tester_extension.dart';
+
+void main() {
+  testWidgets("Test show toast and close toast function",
+      ((WidgetTester test) async {
+    Widget testWidget = Builder(builder: (context) {
+      return Center(
+          child: ElevatedButton(
+        key: const Key("tap-show-toast"),
+        onPressed: () {
+          context.showToast(
+              type: ToastType.errorToast,
+              title: "test title",
+              message: "message");
+        },
+        child: const Text("show toast"),
+      ));
+    });
+
+    await test.wrapAndPump(testWidget);
+    // First: Ensure no toast rendered
+    expect(find.text("test title"), findsNothing);
+    await test.tap(find.byKey(const Key("tap-show-toast")));
+    // Even after tap, toast has not been displayed at this time
+    expect(find.text("test title"), findsNothing);
+    await test
+        .pump(); // schedule animation => Toast should be shown after this time
+    await test.pump(const Duration(milliseconds: 750));
+    expect(find.text("test title"), findsOneWidget);
+    // Second: Toast should dismiss after 6 seconds
+    await test.pump(const Duration(seconds: 6));
+    expect(find.text("test title"), findsNothing);
+
+    // Cover close toast function
+    // Show toast process is the same as the one above
+    expect(find.text("test title"), findsNothing);
+    await test.tap(find.byKey(const Key("tap-show-toast")));
+    expect(find.text("test title"), findsNothing);
+    await test.pump(); // schedule animation
+    await test.pump(const Duration(milliseconds: 750));
+    expect(find.text("test title"), findsOneWidget);
+    // Toast should be shown now then action to close toast
+    await test.tap(find.byKey(const Key("closeTap")));
+    // after closeTap get tapped => widget should not be shown now
+    await test.pump();
+    expect(find.text("test title"), findsNothing);
+    await test.pump(const Duration(seconds: 6));
+    expect(find.text("test title"), findsNothing);
+  }));
+}
