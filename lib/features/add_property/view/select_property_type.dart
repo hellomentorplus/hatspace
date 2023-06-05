4d85fb3972105d1d3b52e52330ac966e40ccdf07
dlvinh <vinhxp95@gmail.com>
2023-06-05 22:45:32 +1000

    temp commit

---
+++ lib/features/add_property/view/select_property_type.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:hatspace/features/add_property/view/add_property_view.dart';
+import 'package:hatspace/features/add_property/view/property_infor_form.dart';
+import 'package:hatspace/features/home/view/home_view.dart';
+import 'package:hatspace/route/router.dart';
+import 'package:hatspace/strings/l10n.dart';
+import 'package:hatspace/theme/hs_theme.dart';
+
+
+class SelectPropertyType extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+     child: Text("Add property type "));
+  }
+}
