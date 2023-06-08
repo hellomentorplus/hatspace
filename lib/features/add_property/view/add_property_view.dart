26d0bd25fb0211920cdc696f1f86f9bb27c575fd
dlvinh <vinhxp95@gmail.com>
2023-06-08 19:02:34 +1000

    remove comment

--- lib/features/add_property/view/add_property_view.dart
+++ lib/features/add_property/view/add_property_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class AddPropertyView extends StatelessWidget {
   const AddPropertyView({Key? key}) : super(key: key);
-  // TODO: Remove
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
