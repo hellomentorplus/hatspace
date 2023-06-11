587b188d72c3e567a349c500d9ce88379b297d1c
dlvinh <vinhxp95@gmail.com>
2023-06-12 09:56:48 +1000

    add widget test

---
+++ test/features/add_property/view/minimum_rent/minimum_rent_period_view_test.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hatspace/features/add_property/view/property_infor/minimum_rent_view.dart';
+import 'package:hatspace/features/add_property/view/property_infor/property_info_form.dart';
+import 'package:hatspace/features/add_property/view_model/cubit/property_infor_cubit.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../../widget_tester_extension.dart';
+import 'minimum_rent_period_view_test.mocks.dart';
+
+@GenerateMocks([PropertyInforCubit])
+void main() {
+  final MockPropertyInforCubit propertyInforCubit = MockPropertyInforCubit();
+  // initializeDateFormatting();
+  setUp(() {
+    when(propertyInforCubit.state)
+        .thenAnswer((realInvocation) => const PropertyInforInitial());
+    when(propertyInforCubit.stream).thenAnswer(
+        (realInvocation) => Stream.value(const PropertyInforInitial()));
+  });
+  testWidgets('test minimum rent period sheet', (widgetTester) async {
+    Widget rentView = MinimumRentView();
+    await widgetTester.blocWrapAndPump<PropertyInforCubit>(
+        propertyInforCubit, rentView);
+    await widgetTester.tap(find.byType(HatSpaceDropDownButton));
+    await widgetTester.pump();
+    expect(find.byKey(const Key("minimum_rent_bottom_sheet")), findsOneWidget);
+  });
+}
