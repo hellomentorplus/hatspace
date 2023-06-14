46c1f69a7675863b46dcbd07289257303c4597ec
dlvinh <vinhxp95@gmail.com>
2023-06-14 23:15:07 +1000

    Squashed commit of the following:
    commit 4ace4300ddbd43011ac23b53c725c9cfaf10836d
    Author: Duc Vinh Le <85751505+dlvinh@users.noreply.github.com>
    Date:   Wed Jun 14 23:12:11 2023 +1000
    
        Hs99 choose kind of place (#69)
    
        * static ui
    
        * update get current date
    
        * add pageview
    
        * add custom datepicker and modal
    
        * custom date picker
    
        * Update date picker
    
        * update button theme
    
        * add next and prev button
    
        * Create hs_date_picker.dart
    
        customise date picker
    
        * add date picker state
    
        * add bloc selector
    
        * Update date_picker_view.dart
    
        * fix bug
    
        * add date picker theme and format files
    
        * fix property page view
    
        * add select_propertt_view_test
    
        * Create date_picker_view_test.dart
    
        * Add bloc test and unit test
    
        * format file
    
        * add pop to home page when close screen
    
        * when close screen then go back to home page
    
        * break down bottom app bar widget
    
        * Update add_property_page_view.dart
    
        * update and fix error
    
        * update bloc cubit
    
        * update state with cubit and validate bottom navbar
    
        * update test coverage
    
        * update test coverage
    
        * update files and update test coverage
    
        * format file
    
        * validate next button even press back to prev screen
    
        * format files
    
        * update test coverage
    
        * update file after merge
    
        * format files
    
        * Resolve
    
        * update sign up
    
        * Replace GridView with Row
    
        * resolve conversation
    
        * format file
    
        * update file

--- lib/features/add_property/view/add_property_view.dart
+++ lib/features/add_property/view/add_property_view.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hatspace/features/add_property/view/select_property_type.dart';
+
+import 'package:hatspace/dimens/hs_dimens.dart';
+import 'package:hatspace/features/add_property/view/property_info_form_view.dart';
 import 'package:hatspace/features/add_property/view_model/add_property_cubit.dart';
 import 'package:hatspace/features/add_property/view_model/add_property_state.dart';
-import 'package:hatspace/features/add_property_info/view/property_info_form.dart';
-import 'package:hatspace/dimens/hs_dimens.dart';
+import 'package:hatspace/features/add_property_type/view/select_property_type.dart';
 import 'package:hatspace/features/add_property_type/view_modal/property_type_cubit.dart';
 
 import 'package:hatspace/gen/assets.gen.dart';
@@ -34,7 +35,12 @@ class AddPropertyPageBody extends StatelessWidget {
       PageController(initialPage: 0, keepPage: true);
   final ValueNotifier<int> onProgressIndicatorState = ValueNotifier(0);
   // Number of Pages for PageView
-  final List<Widget> pages = [const SelectPropertyType(), PropertyInforForm()];
+
+  final List<Widget> pages = [
+    const SelectPropertyType(),
+    const PropertyInforForm()
+  ];
+
   AddPropertyPageBody({super.key});
   @override
   Widget build(BuildContext context) {
