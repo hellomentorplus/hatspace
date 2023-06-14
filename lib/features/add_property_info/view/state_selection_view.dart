edaa30891874b2e6a75f43f851e0dddc5a617b0e
dlvinh <vinhxp95@gmail.com>
2023-06-14 21:04:58 +1000

    update state selection with Value change

--- lib/features/add_property_info/view/state_selection_view.dart
+++ lib/features/add_property_info/view/state_selection_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hatspace/data/property_data.dart';
+import 'package:hatspace/features/add_property/view_model/cubit/add_property_state.dart';
 import 'package:hatspace/features/add_property_info/view/property_info_form.dart';
 import 'package:hatspace/features/add_property_info/view_modal/property_infor_cubit.dart';
 import 'package:hatspace/features/add_property_info/view_modal/property_infor_state.dart';
@@ -30,6 +31,7 @@ class StateSelectionView extends StatelessWidget {
       }
       if (state is SaveSelectedState) {
         label = state.savedState.stateName;
+        context.pop();
       }
       return Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -38,7 +40,6 @@ class StateSelectionView extends StatelessWidget {
               label: HatSpaceStrings.of(context).state, isRequired: true),
           HatSpaceDropDownButton(
               label: label,
-              //TODO: implement state
               isRequired: true,
               onPressed: () {
                 showModalBottomSheet(
@@ -47,129 +48,131 @@ class StateSelectionView extends StatelessWidget {
                       borderRadius: BorderRadius.circular(10.0),
                     ),
                     context: context,
-                    builder: (context) {
-                      return SizedBox(
-                          key: const Key("state_bottom_sheet"),
-                          height: MediaQuery.of(context).size.height * 0.85,
-                          child: Padding(
-                              padding: const EdgeInsets.only(bottom: 42),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    padding: const EdgeInsets.only(
-                                        bottom: 8, top: 24.0),
-                                    decoration: const BoxDecoration(
-                                        border: Border(
-                                            bottom: BorderSide(
-                                                color: HSColor.neutral2))),
-                                    child: Row(
-                                      children: [
-                                        const Expanded(
-                                            child: SizedBox.shrink()),
-                                        Expanded(
-                                            child: Center(
-                                          child: Text(
-                                            HatSpaceStrings.of(context).state,
-                                            style: textTheme.displayLarge
-                                                ?.copyWith(
-                                                    fontSize: FontStyleGuide
-                                                        .fontSize16),
-                                          ),
-                                        )),
-                                        Expanded(
-                                            child: Align(
-                                                alignment:
-                                                    Alignment.centerRight,
-                                                child: IconButton(
-                                                  icon: SvgPicture.asset(
-                                                      Assets.images.closeIcon),
-                                                  onPressed: () {
-                                                    context.pop();
-                                                  },
-                                                )))
-                                      ],
-                                    ),
-                                  ),
-                                  Flexible(
-                                      child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                      horizontal: 24.0,
-                                    ),
-                                    child: ListView.separated(
-                                        separatorBuilder: (context, index) {
-                                          return const Divider(
-                                            thickness: 1.0,
-                                            color: HSColor.neutral3,
-                                          );
-                                        },
-                                        itemCount: stateList.length,
-                                        itemBuilder: (context, index) {
-                                          return GestureDetector(
-                                              onTap: () {
-                                                selectState.value =
-                                                    AustraliaStates
-                                                        .values[index];
-                                              },
-                                              child: ValueListenableBuilder(
-                                                  valueListenable: selectState,
-                                                  builder:
-                                                      (context, value, child) {
-                                                    return Padding(
-                                                        padding:
-                                                            const EdgeInsets
-                                                                    .symmetric(
-                                                                vertical: 16.0),
-                                                        child: Row(
-                                                          mainAxisAlignment:
-                                                              MainAxisAlignment
-                                                                  .spaceBetween,
-                                                          children: [
-                                                            Text(
-                                                              stateList[index]
-                                                                  .stateName,
-                                                            ),
-                                                            value ==
-                                                                    AustraliaStates
-                                                                            .values[
-                                                                        index]
-                                                                ? SvgPicture
-                                                                    .asset(Assets
-                                                                        .images
-                                                                        .check)
-                                                                : const SizedBox
-                                                                    .shrink()
-                                                          ],
-                                                        ));
-                                                  }));
-                                        }),
-                                  )),
-                                  Padding(
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 24.0),
-                                      child: TertiaryButton(
-                                        style: const ButtonStyle(
-                                          foregroundColor:
-                                              MaterialStatePropertyAll<Color>(
-                                                  HSColor.background),
-                                          backgroundColor:
-                                              MaterialStatePropertyAll<Color>(
-                                                  HSColor.green06),
-                                        ),
-                                        label: HatSpaceStrings.of(context).save,
-                                        onPressed: () {
-                                          context.pop();
-                                        },
-                                      ))
-                                ],
-                              )));
-                    }).then((value) {
-                  context
-                      .read<PropertyInforCubit>()
-                      .saveSelectedState(selectState.value);
-                });
+                    builder: (_) {
+                      return StateModal(
+                          selectState: selectState,
+                          stateList: stateList,
+                          onSave: (value) {
+                            context
+                                .read<PropertyInforCubit>()
+                                .saveSelectedState(value);
+                          });
+                    });
               })
         ],
       );
     });
+  }
+}
+
+class StateModal extends StatelessWidget {
+  final ValueNotifier selectState;
+  final List<AustraliaStates> stateList;
+  final ValueChanged? onSave;
+  const StateModal(
+      {super.key,
+      required this.selectState,
+      required this.stateList,
+      this.onSave});
+
+  Widget renderSelectState(AustraliaStates selectedValue, int index) {
+    if (selectedValue == AustraliaStates.values[index]) {
+      return SvgPicture.asset(Assets.images.check);
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return SizedBox(
+        key: const Key("state_bottom_sheet"),
+        height: MediaQuery.of(context).size.height * 0.85,
+        child: Padding(
+            padding: const EdgeInsets.only(bottom: 42),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8, top: 24.0),
+                  decoration: const BoxDecoration(
+                      border:
+                          Border(bottom: BorderSide(color: HSColor.neutral2))),
+                  child: Row(
+                    children: [
+                      const Expanded(child: SizedBox.shrink()),
+                      Expanded(
+                          child: Center(
+                        child: Text(
+                          HatSpaceStrings.of(context).state,
+                          style: textTheme.displayLarge
+                              ?.copyWith(fontSize: FontStyleGuide.fontSize16),
+                        ),
+                      )),
+                      Expanded(
+                          child: Align(
+                              alignment: Alignment.centerRight,
+                              child: IconButton(
+                                icon: SvgPicture.asset(Assets.images.closeIcon),
+                                onPressed: () {
+                                  context.pop();
+                                },
+                              )))
+                    ],
+                  ),
+                ),
+                Flexible(
+                    child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0,
+                  ),
+                  child: ListView.separated(
+                      separatorBuilder: (_, index) {
+                        return const Divider(
+                          thickness: 1.0,
+                          color: HSColor.neutral3,
+                        );
+                      },
+                      itemCount: stateList.length,
+                      itemBuilder: (_, index) {
+                        return GestureDetector(
+                            onTap: () {
+                              selectState.value = AustraliaStates.values[index];
+                            },
+                            child: ValueListenableBuilder(
+                                valueListenable: selectState,
+                                builder: (_, value, child) {
+                                  return Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 16.0),
+                                      child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Text(
+                                            stateList[index].stateName,
+                                          ),
+                                          renderSelectState(value, index)
+                                        ],
+                                      ));
+                                }));
+                      }),
+                )),
+                Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: TertiaryButton(
+                      style: const ButtonStyle(
+                        foregroundColor:
+                            MaterialStatePropertyAll<Color>(HSColor.background),
+                        backgroundColor:
+                            MaterialStatePropertyAll<Color>(HSColor.green06),
+                      ),
+                      label: HatSpaceStrings.of(context).save,
+                      onPressed: () {
+                        onSave!(selectState.value);
+                      },
+                    ))
+              ],
+            )));
   }
 }
