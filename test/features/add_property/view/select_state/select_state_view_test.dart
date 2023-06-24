480e53507f02af2a4234e15140d6f2db8b943482
dlvinh <vinhxp95@gmail.com>
2023-06-25 08:32:11 +1000

    [hs-106] remove cubit files of add property info

--- test/features/add_property/view/select_state/select_state_view_test.dart
+++ test/features/add_property/view/select_state/select_state_view_test.dart
@@ -1,29 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hatspace/features/add_property_info/view/add_property_state_view.dart';
-import 'package:hatspace/features/add_property_info/view_modal/property_infor_cubit.dart';
-import 'package:hatspace/features/add_property_info/view_modal/property_infor_state.dart';
+
 import 'package:hatspace/theme/widgets/hs_buttons.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
 
 import '../../../../widget_tester_extension.dart';
-import 'select_state_view_test.mocks.dart';
 
-@GenerateMocks([PropertyInforCubit])
 void main() {
-  final MockPropertyInforCubit propertyInforCubit = MockPropertyInforCubit();
-  // initializeDateFormatting();
-  setUp(() {
-    when(propertyInforCubit.state)
-        .thenAnswer((realInvocation) => PropertyInforInitial());
-    when(propertyInforCubit.stream)
-        .thenAnswer((realInvocation) => Stream.value(PropertyInforInitial()));
-  });
   testWidgets('test state bottom sheet for widget', (widgetTester) async {
     Widget stateView = AddPropertyStateView();
-    await widgetTester.blocWrapAndPump<PropertyInforCubit>(
-        propertyInforCubit, stateView);
+    await widgetTester.wrapAndPump(stateView);
     await widgetTester.tap(find.byType(HatSpaceDropDownButton));
     await widgetTester.pump();
     expect(find.byKey(const Key('state_view_modal')), findsWidgets);
