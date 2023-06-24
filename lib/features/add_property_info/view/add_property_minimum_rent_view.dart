78471dfe8178179900ccd307c5e8a28562722005
dlvinh <vinhxp95@gmail.com>
2023-06-25 08:26:08 +1000

    [hs-106] - remove cubit out of UI logic

--- lib/features/add_property_info/view/add_property_minimum_rent_view.dart
+++ lib/features/add_property_info/view/add_property_minimum_rent_view.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hatspace/data/property_data.dart';
-import 'package:hatspace/features/add_property_info/view_modal/property_infor_cubit.dart';
-import 'package:hatspace/features/add_property_info/view_modal/property_infor_state.dart';
 import 'package:hatspace/gen/assets.gen.dart';
+import 'package:hatspace/route/router.dart';
 import 'package:hatspace/strings/l10n.dart';
 import 'package:hatspace/theme/widgets/hs_buttons.dart';
 import 'package:hatspace/theme/widgets/hs_modal_view.dart';
@@ -17,51 +15,43 @@ class AddPropertyMinimumView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     periodList.remove(MinimumRentPeriod.invalid);
-    return BlocListener<PropertyInforCubit, PropertyInforState>(
-        listener: (context, state) {
-          if (state is StartListenRentPeriodChange) {
-            // context.pop();
-          }
-        },
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            HatSpaceLabel(
-                label: HatSpaceStrings.of(context).minimumRentPeriod,
-                isRequired: true),
-            ValueListenableBuilder(
-                valueListenable: selectPeriod,
-                builder: (_, value, child) {
-                  return HatSpaceDropDownButton(
-                      icon: Assets.images.chervonDown,
-                      value: selectPeriod.value.displayName,
-                      isRequired: true,
-                      onPressed: () {
-                        showModalBottomSheet(
-                            isScrollControlled: true,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            context: context,
-                            builder: (_) {
-                              return HsModalView(
-                                  key: const Key('rent_view_modal'),
-                                  currentValue: selectPeriod,
-                                  getItemString: (item) => item.displayName,
-                                  itemList: periodList,
-                                  height:
-                                      MediaQuery.of(context).size.height * 0.85,
-                                  title: HatSpaceStrings.of(context)
-                                      .minimumRentPeriod,
-                                  onSave: (value) {
-                                    context
-                                        .read<PropertyInforCubit>()
-                                        .saveMinimumRentPeriod(value);
-                                  });
-                            });
-                      });
-                })
-          ],
-        ));
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        HatSpaceLabel(
+            label: HatSpaceStrings.of(context).minimumRentPeriod,
+            isRequired: true),
+        ValueListenableBuilder(
+            valueListenable: selectPeriod,
+            builder: (_, value, child) {
+              return HatSpaceDropDownButton(
+                  icon: Assets.images.chervonDown,
+                  value: selectPeriod.value.displayName,
+                  isRequired: true,
+                  onPressed: () {
+                    showModalBottomSheet(
+                        isScrollControlled: true,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        context: context,
+                        builder: (_) {
+                          return HsModalView(
+                              key: const Key('rent_view_modal'),
+                              currentValue: selectPeriod,
+                              getItemString: (item) => item.displayName,
+                              itemList: periodList,
+                              height: MediaQuery.of(context).size.height * 0.85,
+                              title:
+                                  HatSpaceStrings.of(context).minimumRentPeriod,
+                              onSave: (value) {
+                                // TODO handle save state on cubit
+                                context.pop();
+                              });
+                        });
+                  });
+            })
+      ],
+    );
   }
 }
