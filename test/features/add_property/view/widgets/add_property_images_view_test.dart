4cb57ae579d0b8dc90724e4b7f1b9e0c7e200faf
Truong Ho Phong <hophongit98@gmail.com>
2023-06-25 15:41:08 +0700

    [HS-117] update test for add property images view

--- test/features/add_property/view/widgets/add_property_images_view_test.dart
+++ test/features/add_property/view/widgets/add_property_images_view_test.dart
@@ -1,19 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hatspace/features/add_property/view/widgets/add_property_images_view.dart';
+import 'package:hatspace/features/add_property/view_model/add_properties_images/add_property_images_cubit.dart';
+import 'package:hatspace/models/permission/permission_service.dart';
+import 'package:hatspace/models/permission/permission_status.dart';
+import 'package:hatspace/singleton/hs_singleton.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../../../widget_tester_extension.dart';
+import 'add_property_images_view_test.mocks.dart';
 
+@GenerateMocks([AddPropertyImagesCubit, HsPermissionService])
 void main() {
-  testWidgets('verify add property images screen with empty selected images',
-      (widgetTester) async {
-    const Widget widget = AddPropertyImagesView();
-    await widgetTester.wrapAndPump(widget);
+  final MockAddPropertyImagesCubit addPropertyImagesCubit =
+      MockAddPropertyImagesCubit();
+  final MockHsPermissionService permissionService = MockHsPermissionService();
 
-    expect(find.byType(Text), findsNWidgets(2));
-    expect(find.text('Let\'s add some photos of your place'), findsOneWidget);
-    expect(find.text('Require at least 4 photos *'), findsOneWidget);
-    expect(find.byType(SvgPicture), findsOneWidget);
+  setUpAll(() {
+    when(addPropertyImagesCubit.state)
+        .thenAnswer((realInvocation) => AddPropertyImagesInitial());
+    when(addPropertyImagesCubit.stream).thenAnswer(
+        (realInvocation) => Stream.value(AddPropertyImagesInitial()));
+    HsSingleton.singleton
+        .registerSingleton<HsPermissionService>(permissionService);
+  });
+
+  tearDown(() {
+    reset(addPropertyImagesCubit);
+    reset(permissionService);
+  });
+
+  group('verify UI', () {
+    testWidgets(
+        'given state is AddPropertyImagesInitial '
+        'when launching AddPropertyImagesView '
+        'then verify AddPropertyImagesView with empty selected images',
+        (widgetTester) async {
+      const Widget widget = AddPropertyImagesView();
+
+      await widgetTester.blocWrapAndPump<AddPropertyImagesCubit>(
+          addPropertyImagesCubit, widget);
+
+      expect(find.byType(Text), findsNWidgets(2));
+      expect(find.text('Let\'s add some photos of your place'), findsOneWidget);
+      expect(find.text('Require at least 4 photos *'), findsOneWidget);
+      SvgPicture uploadPhoto = widgetTester.widget(find.descendant(
+          of: find.byType(AddPropertyImagesView),
+          matching: find.byType(SvgPicture)));
+      BytesLoader bytesLoader = uploadPhoto.bytesLoader;
+      expect(bytesLoader, isA<SvgAssetLoader>());
+      expect((bytesLoader as SvgAssetLoader).assetName,
+          'assets/images/upload_photo.svg');
+    });
+
+    testWidgets(
+        'given state is PhotoPermissionGranted '
+        'when launching AddPropertyImagesView '
+        'then verify AddPropertyImagesView with empty selected images',
+        (widgetTester) async {
+      const Widget widget = AddPropertyImagesView();
+
+      when(addPropertyImagesCubit.state)
+          .thenAnswer((realInvocation) => PhotoPermissionGranted());
+      when(addPropertyImagesCubit.stream).thenAnswer(
+          (realInvocation) => Stream.value(PhotoPermissionGranted()));
+
+      await widgetTester.blocWrapAndPump<AddPropertyImagesCubit>(
+          addPropertyImagesCubit, widget);
+
+      expect(find.byType(Text), findsNWidgets(2));
+      expect(find.text('Let\'s add some photos of your place'), findsOneWidget);
+      expect(find.text('Require at least 4 photos *'), findsOneWidget);
+      SvgPicture uploadPhoto = widgetTester.widget(find.descendant(
+          of: find.byType(AddPropertyImagesView),
+          matching: find.byType(SvgPicture)));
+      BytesLoader bytesLoader = uploadPhoto.bytesLoader;
+      expect(bytesLoader, isA<SvgAssetLoader>());
+      expect((bytesLoader as SvgAssetLoader).assetName,
+          'assets/images/upload_photo.svg');
+    });
+
+    testWidgets(
+        'given state is PhotoPermissionDenied '
+        'when launching AddPropertyImagesView '
+        'then verify AddPropertyImagesView with empty selected images',
+        (widgetTester) async {
+      const Widget widget = AddPropertyImagesView();
+
+      when(addPropertyImagesCubit.state)
+          .thenAnswer((realInvocation) => PhotoPermissionDenied());
+      when(addPropertyImagesCubit.stream).thenAnswer(
+          (realInvocation) => Stream.value(PhotoPermissionDenied()));
+
+      await widgetTester.blocWrapAndPump<AddPropertyImagesCubit>(
+          addPropertyImagesCubit, widget);
+
+      expect(find.byType(Text), findsNWidgets(2));
+      expect(find.text('Let\'s add some photos of your place'), findsOneWidget);
+      expect(find.text('Require at least 4 photos *'), findsOneWidget);
+      SvgPicture uploadPhoto = widgetTester.widget(find.descendant(
+          of: find.byType(AddPropertyImagesView),
+          matching: find.byType(SvgPicture)));
+      BytesLoader bytesLoader = uploadPhoto.bytesLoader;
+      expect(bytesLoader, isA<SvgAssetLoader>());
+      expect((bytesLoader as SvgAssetLoader).assetName,
+          'assets/images/upload_photo.svg');
+    });
+
+    testWidgets(
+        'given state is PhotoPermissionDeniedForever '
+        'when launching AddPropertyImagesView '
+        'then verify AddPropertyImagesView with empty selected images',
+        (widgetTester) async {
+      const Widget widget = AddPropertyImagesView();
+
+      when(addPropertyImagesCubit.state)
+          .thenAnswer((realInvocation) => PhotoPermissionDeniedForever());
+      when(addPropertyImagesCubit.stream).thenAnswer(
+          (realInvocation) => Stream.value(PhotoPermissionDeniedForever()));
+
+      await widgetTester.blocWrapAndPump<AddPropertyImagesCubit>(
+          addPropertyImagesCubit, widget);
+
+      expect(find.byType(Text), findsNWidgets(2));
+      expect(find.text('Let\'s add some photos of your place'), findsOneWidget);
+      expect(find.text('Require at least 4 photos *'), findsOneWidget);
+      SvgPicture uploadPhoto = widgetTester.widget(find.descendant(
+          of: find.byType(AddPropertyImagesView),
+          matching: find.byType(SvgPicture)));
+      BytesLoader bytesLoader = uploadPhoto.bytesLoader;
+      expect(bytesLoader, isA<SvgAssetLoader>());
+      expect((bytesLoader as SvgAssetLoader).assetName,
+          'assets/images/upload_photo.svg');
+    });
+  });
+
+  group('verify interaction', () {
+    testWidgets(
+        'given photo permission is denied '
+        'when taps on upload photo '
+        'then permission service should request photo permission',
+        (widgetTester) async {
+      const Widget widget = AddPropertyImagesView();
+
+      await widgetTester.blocWrapAndPump<AddPropertyImagesCubit>(
+        addPropertyImagesCubit,
+        widget,
+      );
+
+      Finder uploadPhoto = find.descendant(
+        of: find.byType(InkWell),
+        matching: find.byType(SvgPicture),
+      );
+      expect(uploadPhoto, findsOneWidget);
+      
+      await widgetTester.tap(uploadPhoto);
+      await widgetTester.pumpAndSettle();
+
+      verify(addPropertyImagesCubit.requestPhotoPermission()).called(1);
+      verifyNoMoreInteractions(addPropertyImagesCubit);
+    });
   });
 }
