4c4186290dc3833e6c4584dfee86b0c23854e410
Truong Ho Phong <hophongit98@gmail.com>
2023-06-25 17:14:16 +0700

    [HS-117] add permission service test

--- lib/models/permission/permission_service.dart
+++ lib/models/permission/permission_service.dart
@@ -1,27 +1,10 @@
-import 'package:device_info/device_info.dart';
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/foundation.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 import 'package:hatspace/models/permission/permission_status.dart';
 
 class HsPermissionService {
-  Future<HsPermissionStatus> checkPhotoPermission() async {
-    PermissionStatus status;
-    if (defaultTargetPlatform == TargetPlatform.android) {
-      final androidInfo = await DeviceInfoPlugin().androidInfo;
-
-      if (androidInfo.version.sdkInt <= 32) {
-        status = await Permission.storage.status;
-      } else {
-        status = await Permission.photos.status;
-      }
-    } else {
-      status = await Permission.photos.status;
-    }
-
-    return _toHsPermissionStatus(status);
-  }
-
   Future<HsPermissionStatus> requestPhotoPermission() async {
     PermissionStatus status;
     if (defaultTargetPlatform == TargetPlatform.android) {
