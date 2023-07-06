6b8e7b7ba4751b25d156edbdcb3d9c240ff9e746
dlvinh <vinhxp95@gmail.com>
2023-07-06 21:44:10 +1000

    [hs-155-part-1] update hs_dimens and replace homeview icAgen icon

--- lib/dimens/hs_dimens.dart
+++ lib/dimens/hs_dimens.dart
@@ -46,8 +46,8 @@ class HsDimens {
 
   // radius
   static const double radius2 = 2.0;
-  static const double radius7 = 7.0;
   static const double radius4 = 4.0;
+  static const double radius7 = 7.0;
   static const double radius8 = 8.0;
   static const double radius12 = 12.0;
   static const double radius16 = 16.0;
