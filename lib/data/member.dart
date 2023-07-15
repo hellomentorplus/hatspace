089655c355bd721210703ad54517ea22859a331f
dlvinh <vinhxp95@gmail.com>
2023-07-16 08:27:01 +1000

    [hs-210] update private type for variable and add Map data type for method

--- lib/data/member.dart
+++ lib/data/member.dart
@@ -1,16 +1,18 @@
 import 'package:hatspace/data/data.dart';
 
 class Member {
-  final rolesKey = 'roles';
-  final displayNameKey = 'displayName';
+  final String _rolesKey = 'roles';
+  final String _displayNameKey = 'displayName';
   final Set<Roles> listRoles;
   final String displayName;
 
   Member({required this.listRoles, required this.displayName});
-  convertToMap() {
+  
+  //Allow to convert to Map type, which is used to store on Firestore
+  Map<String,dynamic> convertToMap() {
     return {
-      rolesKey: listRoles.map((e) => e.name).toList(),
-      displayNameKey: displayName
+      _rolesKey: listRoles.map((e) => e.name).toList(),
+      _displayNameKey: displayName
     };
   }
 }
