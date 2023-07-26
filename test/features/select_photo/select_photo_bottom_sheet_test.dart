b6f5844ee1606133f291b96f49749e91eb409ebf
DongNguyenQ <41529808+DongNguyenQ@users.noreply.github.com>
2023-07-26 20:49:21 +0700

    [HS-213] Handle select images button (#120)
    * [HS213] Add button callback and tests
    
    * [HS213] Fixed conflicts

--- test/features/select_photo/select_photo_bottom_sheet_test.dart
+++ test/features/select_photo/select_photo_bottom_sheet_test.dart
@@ -6,17 +6,26 @@ import 'package:hatspace/features/select_photo/view/select_photo_bottom_sheet.da
 import 'package:hatspace/features/select_photo/view_model/lost_data_bottom_sheet_cubit.dart';
 import 'package:hatspace/features/select_photo/view_model/photo_selection_cubit.dart';
 import 'package:hatspace/features/select_photo/view_model/select_photo_cubit.dart';
+import 'package:hatspace/models/photo/photo_service.dart';
+import 'package:hatspace/singleton/hs_singleton.dart';
 import 'package:hatspace/strings/l10n.dart';
 import 'package:hatspace/theme/widgets/hs_buttons.dart';
 import 'package:hatspace/theme/widgets/hs_warning_bottom_sheet.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+import 'package:photo_manager/photo_manager.dart';
 
 import '../../widget_tester_extension.dart';
 import 'select_photo_bottom_sheet_test.mocks.dart';
 
-@GenerateMocks(
-    [SelectPhotoCubit, PhotoSelectionCubit, LostDataBottomSheetCubit])
+@GenerateMocks([
+  SelectPhotoCubit,
+  PhotoSelectionCubit,
+  LostDataBottomSheetCubit,
+  PhotoService
+])
+@GenerateNiceMocks([MockSpec<AssetEntity>()])
 void main() async {
   await HatSpaceStrings.load(const Locale('en'));
 
@@ -24,6 +33,11 @@ void main() async {
   final MockPhotoSelectionCubit photoSelectionCubit = MockPhotoSelectionCubit();
   final MockLostDataBottomSheetCubit lostDataBottomSheetCubit =
       MockLostDataBottomSheetCubit();
+  final MockPhotoService photoService = MockPhotoService();
+
+  setUpAll(() {
+    HsSingleton.singleton.registerSingleton<PhotoService>(photoService);
+  });
 
   setUp(() {
     when(selectPhotoCubit.state).thenReturn(SelectPhotoInitial());
@@ -231,5 +245,148 @@ void main() async {
       // select photo bottom sheet is dismissed
       expect(find.byType(SelectPhotoBottomSheet), findsNothing);
     });
+  });
+
+  testWidgets(
+      "given user has images on their device but user hasn't selected any image,"
+      'when load AllPhotosView,'
+      'then the the Selected Image button in GridView is disabled',
+      (widgetTester) async {
+    when(selectPhotoCubit.state).thenAnswer((realInvocation) => PhotosLoaded([
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity()
+        ]));
+    when(photoSelectionCubit.state)
+        .thenAnswer((realInvocation) => const PhotoSelectionUpdated({}, false));
+
+    const Widget widget = AllPhotosView();
+
+    await mockNetworkImagesFor(() => widgetTester.multiBlocWrapAndPump([
+          BlocProvider<SelectPhotoCubit>(
+            create: (context) => selectPhotoCubit,
+          ),
+          BlocProvider<PhotoSelectionCubit>(
+            create: (context) => photoSelectionCubit,
+          )
+        ], widget));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(GridView), findsOneWidget);
+
+    final Finder btnFinder = find.byType(PrimaryButton);
+    expect(btnFinder, findsOneWidget);
+    final PrimaryButton buttonWidget = widgetTester.widget(btnFinder);
+    expect(buttonWidget.onPressed, null);
+  });
+
+  testWidgets(
+      'given user has images on their device but user has selected 3 images,'
+      'when load AllPhotosView,'
+      'then the the Selected Image button in GridView is disabled',
+      (widgetTester) async {
+    when(selectPhotoCubit.state).thenAnswer((realInvocation) => PhotosLoaded([
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity()
+        ]));
+    when(photoSelectionCubit.state).thenAnswer((realInvocation) =>
+        const PhotoSelectionUpdated({'a', 'b', 'c'}, false));
+
+    const Widget widget = AllPhotosView();
+
+    await mockNetworkImagesFor(() => widgetTester.multiBlocWrapAndPump([
+          BlocProvider<SelectPhotoCubit>(
+            create: (context) => selectPhotoCubit,
+          ),
+          BlocProvider<PhotoSelectionCubit>(
+            create: (context) => photoSelectionCubit,
+          )
+        ], widget));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(GridView), findsOneWidget);
+
+    final Finder btnFinder = find.byType(PrimaryButton);
+    expect(btnFinder, findsOneWidget);
+    final PrimaryButton buttonWidget = widgetTester.widget(btnFinder);
+    expect(buttonWidget.onPressed, null);
+  });
+
+  testWidgets(
+      'given user has images on their device but user has selected 4 images,'
+      'when load AllPhotosView,'
+      'then the Selected Image button in GridView is enabled',
+      (widgetTester) async {
+    when(selectPhotoCubit.state).thenAnswer((realInvocation) => PhotosLoaded([
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity()
+        ]));
+    when(photoSelectionCubit.state).thenAnswer((realInvocation) =>
+        const PhotoSelectionUpdated({'a', 'b', 'c', 'd'}, true));
+
+    const Widget widget = AllPhotosView();
+
+    await mockNetworkImagesFor(() => widgetTester.multiBlocWrapAndPump([
+          BlocProvider<SelectPhotoCubit>(
+            create: (context) => selectPhotoCubit,
+          ),
+          BlocProvider<PhotoSelectionCubit>(
+            create: (context) => photoSelectionCubit,
+          )
+        ], widget));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(GridView), findsOneWidget);
+
+    final Finder btnFinder = find.byType(PrimaryButton);
+    expect(btnFinder, findsOneWidget);
+    final PrimaryButton buttonWidget = widgetTester.widget(btnFinder);
+    expect(buttonWidget.onPressed != null, true);
+  });
+
+  testWidgets(
+      'given user has selected 4 images,'
+      'when user tap on submit button,'
+      'then the select images bottom sheet will be closed.',
+      (widgetTester) async {
+    when(selectPhotoCubit.state).thenAnswer((realInvocation) => PhotosLoaded([
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity(),
+          MockAssetEntity()
+        ]));
+    when(photoSelectionCubit.state).thenAnswer((realInvocation) =>
+        const PhotoSelectionUpdated({'a', 'b', 'c', 'd'}, true));
+
+    const Widget widget = AllPhotosView();
+
+    await mockNetworkImagesFor(() => widgetTester.multiBlocWrapAndPump([
+          BlocProvider<SelectPhotoCubit>(
+            create: (context) => selectPhotoCubit,
+          ),
+          BlocProvider<PhotoSelectionCubit>(
+            create: (context) => photoSelectionCubit,
+          )
+        ], widget));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(GridView), findsOneWidget);
+
+    final Finder btnFinder = find.byType(PrimaryButton);
+    expect(btnFinder, findsOneWidget);
+
+    await widgetTester.tap(btnFinder);
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(SelectPhotoBottomSheet), findsNothing);
   });
 }
