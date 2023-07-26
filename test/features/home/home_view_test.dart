d2ab7668f3077adc5292788af8c49e491596c487
suesitran <17781268+suesitran@users.noreply.github.com>
2023-07-26 17:26:25 +0700

    HS-159 format code

--- test/features/home/home_view_test.dart
+++ test/features/home/home_view_test.dart
@@ -91,7 +91,8 @@ void main() {
     when(authenticationBloc.stream)
         .thenAnswer((realInvocation) => const Stream.empty());
 
-    when(getPropertiesCubit.state).thenReturn(const GetPropertiesInitialState());
+    when(getPropertiesCubit.state)
+        .thenReturn(const GetPropertiesInitialState());
     when(getPropertiesCubit.stream)
         .thenAnswer((realInvocation) => const Stream.empty());
 
