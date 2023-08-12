dc5ff44489a3bd052d228db894ed3b5d9f0663e7
Truong Ho Phong <hophongit98@gmail.com>
2023-08-12 23:42:53 +0700

    [hs-319] add UI test

---
+++ test/features/inspection/inspection_view_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hatspace/features/inspection/inspection_view.dart';
+import 'package:network_image_mock/network_image_mock.dart';
+
+import '../../widget_tester_extension.dart';
+
+void main() {
+  testWidgets('verify UI', (widgetTester) async {
+    await mockNetworkImagesFor(() =>  widgetTester.wrapAndPump(const InspectionView()));
+
+    expect(find.text('Inspection Booking'), findsOneWidget);
+    expect(find.text('3 inspect booking'), findsOneWidget);
+    expect(find.byType(TenantBookItemView), findsNWidgets(3));
+    // skip check dummy data, will update when getting real data
+  });
+}
