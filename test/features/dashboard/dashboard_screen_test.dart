2d830d28d558e94b83b5d54ac2f166cb5e4cc9a8
DongNguyenQ <41529808+DongNguyenQ@users.noreply.github.com>
2023-08-20 11:19:55 +0700

    [HS-317] Fix analysis

--- test/features/dashboard/dashboard_screen_test.dart
+++ test/features/dashboard/dashboard_screen_test.dart
@@ -3,12 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hatspace/data/data.dart';
+import 'package:hatspace/features/application/view/application_guide_screen.dart';
 import 'package:hatspace/features/dashboard/dashboard_screen.dart';
 import 'package:hatspace/features/dashboard/view_model/add_home_owner_role_cubit.dart';
 import 'package:hatspace/features/dashboard/view_model/dashboard_interaction_cubit.dart';
 import 'package:hatspace/features/home/view/home_view.dart';
 import 'package:hatspace/features/inspection/inspection_view.dart';
-import 'package:hatspace/features/application/application_view.dart';
 import 'package:hatspace/features/profile/view/profile_view.dart';
 import 'package:hatspace/gen/assets.gen.dart';
 import 'package:hatspace/models/authentication/authentication_service.dart';
@@ -249,7 +249,7 @@ void main() {
 
         // login bottom sheet is not displayed
         expect(find.byType(HsWarningBottomSheetView), findsNothing);
-        expect(find.byType(ApplicationView), findsOneWidget);
+        expect(find.byType(ApplicationGuideScreen), findsOneWidget);
       });
     });
   });
