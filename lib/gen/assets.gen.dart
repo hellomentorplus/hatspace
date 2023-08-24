fc616dca63018aaf09220291fafa5abd56b3bbfd
Duc Vinh Le <85751505+dlvinh@users.noreply.github.com>
2023-08-24 17:16:42 +1000

    [Hs-325] success booking inspection UI (#180)
    * [hs-325] Update BookedItemCard
    
    * [hs-325] add success screen
    
    * [hs-325] add widget tests
    
    * [hs325] implement cubit to booking screen
    
    * [hs-325] add cubit to screen
    
    * [hs-325] add bloc test
    
    * [hs325] format files
    
    * [hs-325] remove duplicate icon
    
    * Revert "[hs-325] add success screen"
    
    This reverts commit e689889e748ce1dfd7f237ed220022b8c3d5f3aa.
    
    * Revert "Revert "[hs-325] add success screen""
    
    This reverts commit 06b9fb9a30ea452b25f611b05a961434e2a91f00.
    
    * [hs-325] update date format using arb file
    
    * [hs-325] format file
    
    * [hs-325] update ui
    
    * [hs325] rename function
    
    * [hs-325] format files

--- lib/gen/assets.gen.dart
+++ lib/gen/assets.gen.dart
@@ -195,6 +195,9 @@ class $AssetsIconsGen {
   /// File path: assets/icons/portable_fans.svg
   String get portableFans => 'assets/icons/portable_fans.svg';
 
+  /// File path: assets/icons/primary_check.svg
+  String get primaryCheck => 'assets/icons/primary_check.svg';
+
   /// File path: assets/icons/profile-circle.svg
   String get profileCircle => 'assets/icons/profile-circle.svg';
 
@@ -297,6 +300,7 @@ class $AssetsIconsGen {
         phone,
         photoAccess,
         portableFans,
+        primaryCheck,
         profileCircle,
         profile,
         requestHomeownerRole,
@@ -350,10 +354,6 @@ class $AssetsImagesGen {
   AssetGenImage get signInBackground =>
       const AssetGenImage('assets/images/signIn_background.png');
 
-  /// File path: assets/images/tempImage.png
-  AssetGenImage get tempImage =>
-      const AssetGenImage('assets/images/tempImage.png');
-
   /// File path: assets/images/upload.svg
   String get upload => 'assets/images/upload.svg';
 
@@ -376,7 +376,6 @@ class $AssetsImagesGen {
         loginCircle,
         logo,
         signInBackground,
-        tempImage,
         upload,
         uploadPhoto,
         userDefaultAvatar
