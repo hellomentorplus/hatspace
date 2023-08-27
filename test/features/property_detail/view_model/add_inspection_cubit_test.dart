ab8e0b613d236162af5222231e66da3f0c55f696
dlvinh <vinhxp95@gmail.com>
2023-08-27 21:52:03 +1000

    [hs-379] add bloc tests and widget tests

--- test/features/property_detail/view_model/add_inspection_cubit_test.dart
+++ test/features/property_detail/view_model/add_inspection_cubit_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hatspace/data/data.dart';
 import 'package:hatspace/features/booking/view_model/cubit/add_inspection_booking_cubit.dart';
+import 'package:hatspace/features/property_detail/view_model/property_detail_cubit.dart';
 import 'package:hatspace/models/authentication/authentication_service.dart';
 import 'package:hatspace/models/storage/member_service/member_storage_service.dart';
 import 'package:hatspace/models/storage/storage_service.dart';
@@ -24,15 +25,16 @@ void main() async {
   final MockMemberService mockMemberService = MockMemberService();
   setUpAll(() async {
     when(storageServiceMock.member).thenReturn(mockMemberService);
-  });
-
-  setUp(() {
     HsSingleton.singleton
         .registerSingleton<AuthenticationService>(authenticatioServiceMock);
     HsSingleton.singleton.registerSingleton<StorageService>(storageServiceMock);
   });
 
-  tearDown(() {});
+  tearDown(() {
+    reset(authenticatioServiceMock);
+    reset(storageServiceMock);
+    reset(mockMemberService);
+  });
 
   blocTest<AddInspectionBookingCubit, AddInspectionBookingState>(
       'Given user is booking an inspection'
@@ -51,4 +53,27 @@ void main() async {
       },
       act: (bloc) => bloc.onBookInspection(),
       expect: () => [BookingInspectionSuccess()]);
+
+  blocTest<PropertyDetailCubit, PropertyDetailState>(
+    'when trigger navigate to booking inspection'
+    'then return NavigateToBookingInspectionScreen state',
+    build: () => PropertyDetailCubit(),
+    setUp: () {
+      when(authenticatioServiceMock.isUserLoggedIn).thenReturn(true);
+    },
+    act: (bloc) => bloc.navigateToBooingInspectionScreen(),
+    expect: () => [NavigateToBooingInspectionScreen()],
+  );
+
+  blocTest<PropertyDetailCubit, PropertyDetailState>(
+    'Given when user has not logged in'
+    'when user trigger navigate to booking screen'
+    'then return nothing',
+    build: () => PropertyDetailCubit(),
+    setUp: () {
+      when(authenticatioServiceMock.isUserLoggedIn).thenReturn(false);
+    },
+    act: (bloc) => bloc.navigateToBooingInspectionScreen(),
+    expect: () => [],
+  );
 }
