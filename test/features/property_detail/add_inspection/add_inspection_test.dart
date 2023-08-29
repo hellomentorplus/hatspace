d4fe7ba00e77a62b5f77d6948e0d8d48b271c103
DongNguyenQ <41529808+DongNguyenQ@users.noreply.github.com>
2023-08-29 10:40:03 +0700

    [HS-378] Fixed bug user has 2 roles but can't navigate to success screen (#188)
    * [HS-378] Fixed bug user has 2 roles but can't navigate to success screen
    
    * [HS-378] Added more test
    
    * [HS-378] Added more test

--- test/features/property_detail/add_inspection/add_inspection_test.dart
+++ test/features/property_detail/add_inspection/add_inspection_test.dart
@@ -1,35 +1,43 @@
+import 'package:clock/clock.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hatspace/features/booking/add_inspection_booking_screen.dart';
 import 'package:hatspace/features/booking/add_inspection_success_booking_screen.dart';
+import 'package:hatspace/features/booking/view_model/cubit/add_inspection_booking_cubit.dart';
 import 'package:hatspace/gen/assets.gen.dart';
 import 'package:hatspace/models/authentication/authentication_service.dart';
 import 'package:hatspace/models/storage/storage_service.dart';
 import 'package:hatspace/singleton/hs_singleton.dart';
 import 'package:hatspace/strings/l10n.dart';
 import 'package:hatspace/theme/widgets/hs_buttons.dart';
+import 'package:hatspace/theme/widgets/hs_date_picker.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 import 'package:network_image_mock/network_image_mock.dart';
 import '../../../find_extension.dart';
 import '../../../widget_tester_extension.dart';
-import '../property_detail_screen_test.mocks.dart';
+import '../../add_property/view/widgets/add_rooms_view_test.dart';
+import 'add_inspection_test.mocks.dart';
 
-@GenerateMocks([
-  StorageService,
-  AuthenticationService,
-])
+@GenerateMocks(
+    [StorageService, AuthenticationService, AddInspectionBookingCubit])
 void main() async {
   HatSpaceStrings.load(const Locale('en'));
   initializeDateFormatting();
-  final MockAuthenticationService authenticatioServiceMock =
+  final MockAuthenticationService authenticationServiceMock =
       MockAuthenticationService();
   final MockStorageService storageServiceMock = MockStorageService();
+  final MockAddInspectionBookingCubit addInspectionBookingCubit =
+      MockAddInspectionBookingCubit();
 
   setUpAll(() async {
     HsSingleton.singleton
-        .registerSingleton<AuthenticationService>(authenticatioServiceMock);
+        .registerSingleton<AuthenticationService>(authenticationServiceMock);
     HsSingleton.singleton.registerSingleton<StorageService>(storageServiceMock);
+    withClock(Clock.fixed(DateTime(2022, 10, 15)), () async {
+      HsSingleton.singleton.registerSingleton<Clock>(clock);
+    });
   });
 
   testWidgets('Verify UI component', (WidgetTester widget) async {
@@ -55,6 +63,82 @@ void main() async {
     await widgetTester.pumpAndSettle();
     // Navigate to other screen
     expect(find.byType(AddInspectionBookingScreen), findsNothing);
+  });
+
+  testWidgets(
+      'Given user is in AddInspectionBookingScreen. '
+      'When user tap on booking button. '
+      'Then user will start booking process.', (widgetTester) async {
+    when(addInspectionBookingCubit.stream)
+        .thenAnswer((_) => Stream.value(AddInspectionBookingInitial()));
+    when(addInspectionBookingCubit.state)
+        .thenAnswer((_) => AddInspectionBookingInitial());
+
+    await mockNetworkImagesFor(() =>
+        widgetTester.blocWrapAndPump<AddInspectionBookingCubit>(
+            addInspectionBookingCubit, AddInspectionBookingBody()));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(AddInspectionBookingBody), findsOneWidget);
+
+    final Finder bookingBtnFinder = find.byType(PrimaryButton);
+
+    expect(bookingBtnFinder, findsOneWidget);
+
+    await widgetTester.ensureVisible(bookingBtnFinder);
+    await widgetTester.tap(bookingBtnFinder);
+    await widgetTester.pumpAndSettle();
+
+    verify(addInspectionBookingCubit.onBookInspection()).called(1);
+  });
+
+  testWidgets(
+      'Given user is in AddInspectionBookingScreen. '
+      'When user tap on the date picker. '
+      'Then the date picker ui will be shown up.', (widgetTester) async {
+    when(addInspectionBookingCubit.stream)
+        .thenAnswer((_) => Stream.value(AddInspectionBookingInitial()));
+    when(addInspectionBookingCubit.state)
+        .thenAnswer((_) => AddInspectionBookingInitial());
+
+    await mockNetworkImagesFor(() =>
+        widgetTester.blocWrapAndPump<AddInspectionBookingCubit>(
+            addInspectionBookingCubit, AddInspectionBookingBody()));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(AddInspectionBookingBody), findsOneWidget);
+
+    final Finder datePickerFinder = find.ancestor(
+        of: find.byWidgetPredicate((widget) =>
+            validateSvgPictureWithAssets(widget, 'assets/icons/calendar.svg')),
+        matching: find.byType(SecondaryButton));
+
+    expect(datePickerFinder, findsOneWidget);
+
+    await widgetTester.ensureVisible(datePickerFinder);
+    await widgetTester.tap(datePickerFinder);
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(HsDatePicker), findsOneWidget);
+  });
+
+  testWidgets(
+      'Given user is in AddInspectionBookingScreen. '
+      'When booking action was succeed. '
+      'Then user will be navigated out of AddInspectionBookingScreen',
+      (widgetTester) async {
+    when(addInspectionBookingCubit.stream)
+        .thenAnswer((_) => Stream.value(BookingInspectionSuccess()));
+    when(addInspectionBookingCubit.state)
+        .thenAnswer((_) => BookingInspectionSuccess());
+
+    await mockNetworkImagesFor(() =>
+        widgetTester.blocWrapAndPump<AddInspectionBookingCubit>(
+            addInspectionBookingCubit, AddInspectionBookingBody()));
+    await widgetTester.pumpAndSettle();
+
+    expect(find.byType(AddInspectionBookingBody), findsNothing);
+    expect(find.byType(AddInspectionSuccessScreen), findsOneWidget);
   });
 
   group('Booking inspection success test cases', () {
--- test/features/property_detail/view_model/add_inspection_cubit_test.dart
+++ test/features/property_detail/view_model/add_inspection_cubit_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hatspace/data/data.dart';
 import 'package:hatspace/features/booking/view_model/cubit/add_inspection_booking_cubit.dart';
+import 'package:hatspace/models/authentication/authentication_exception.dart';
 import 'package:hatspace/models/authentication/authentication_service.dart';
 import 'package:hatspace/models/storage/member_service/member_storage_service.dart';
 import 'package:hatspace/models/storage/storage_service.dart';
@@ -12,27 +13,22 @@ import 'package:intl/date_symbol_data_local.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import '../property_detail_screen_test.mocks.dart';
+import 'add_inspection_cubit_test.mocks.dart';
 
-@GenerateMocks([MemberService])
+@GenerateMocks([MemberService, AuthenticationService, StorageService])
 void main() async {
   HatSpaceStrings.load(const Locale('en'));
   initializeDateFormatting();
-  final MockAuthenticationService authenticatioServiceMock =
+  final MockAuthenticationService authenticationServiceMock =
       MockAuthenticationService();
   final MockStorageService storageServiceMock = MockStorageService();
   final MockMemberService mockMemberService = MockMemberService();
   setUpAll(() async {
+    HsSingleton.singleton
+        .registerSingleton<AuthenticationService>(authenticationServiceMock);
+    HsSingleton.singleton.registerSingleton<StorageService>(storageServiceMock);
     when(storageServiceMock.member).thenReturn(mockMemberService);
   });
-
-  setUp(() {
-    HsSingleton.singleton
-        .registerSingleton<AuthenticationService>(authenticatioServiceMock);
-    HsSingleton.singleton.registerSingleton<StorageService>(storageServiceMock);
-  });
-
-  tearDown(() {});
 
   blocTest<AddInspectionBookingCubit, AddInspectionBookingState>(
       'Given user is booking an inspection'
@@ -40,7 +36,7 @@ void main() async {
       'Then return SuccessBookInspection',
       build: () => AddInspectionBookingCubit(),
       setUp: () {
-        when(authenticatioServiceMock.getCurrentUser())
+        when(authenticationServiceMock.getCurrentUser())
             .thenAnswer((realInvocation) {
           return Future.value(UserDetail(uid: 'uid'));
         });
@@ -51,4 +47,62 @@ void main() async {
       },
       act: (bloc) => bloc.onBookInspection(),
       expect: () => [BookingInspectionSuccess()]);
+
+  blocTest<AddInspectionBookingCubit, AddInspectionBookingState>(
+      'Given user is booking an inspection'
+      'When user already had tenant and homeowner role'
+      'Then return SuccessBookInspection',
+      build: () => AddInspectionBookingCubit(),
+      setUp: () {
+        when(authenticationServiceMock.getCurrentUser())
+            .thenAnswer((realInvocation) {
+          return Future.value(UserDetail(uid: 'uid'));
+        });
+        when(mockMemberService.getUserRoles('uid'))
+            .thenAnswer((realInvocation) {
+          return Future.value([Roles.tenant, Roles.homeowner]);
+        });
+      },
+      act: (bloc) => bloc.onBookInspection(),
+      expect: () => [BookingInspectionSuccess()]);
+
+  blocTest<AddInspectionBookingCubit, AddInspectionBookingState>(
+      'Given user is booking an inspection'
+      'When user only had homeowner role'
+      'Then do not emit success state',
+      build: () => AddInspectionBookingCubit(),
+      setUp: () {
+        when(authenticationServiceMock.getCurrentUser())
+            .thenAnswer((realInvocation) {
+          return Future.value(UserDetail(uid: 'uid'));
+        });
+        when(mockMemberService.getUserRoles('uid'))
+            .thenAnswer((realInvocation) {
+          return Future.value([Roles.homeowner]);
+        });
+      },
+      act: (bloc) => bloc.onBookInspection(),
+      expect: () => []);
+
+  blocTest<AddInspectionBookingCubit, AddInspectionBookingState>(
+      'Given authentication service failed to get current user. '
+      'When user is booking an inspection. '
+      'Then UserNotFoundException will be thrown.',
+      build: () => AddInspectionBookingCubit(),
+      setUp: () {
+        when(authenticationServiceMock.getCurrentUser())
+            .thenThrow(UserNotFoundException());
+      },
+      act: (bloc) => bloc.onBookInspection(),
+      verify: (_) => [isA<UserNotFoundException>()]);
+
+  blocTest<AddInspectionBookingCubit, AddInspectionBookingState>(
+      'Given AddInspectionBookingCubit was just created. '
+      'Then state will be AddInspectionBookingInitial.',
+      build: () => AddInspectionBookingCubit(),
+      act: (bloc) => bloc.onBookInspection(),
+      verify: (bloc) {
+        expect(bloc.state is AddInspectionBookingInitial, true);
+        expect((bloc.state as AddInspectionBookingInitial).props, []);
+      });
 }
